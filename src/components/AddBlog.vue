b454cd458ee63f524749eb398302664299be8a84
unknown <1159658829@qq.com>
2020-03-28 17:29:12 +0800

    --- Auto-Git Commit ---

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -61,11 +61,13 @@ export default {
         post:function(){
             var that = this
             // this.$axios.post('https://jsonplaceholder.typicode.com/posts',{
-            this.$axios.post('https://my-vuedemo.firebaseio.com/posts.json',{
-                title:this.blog.title,
-                content:this.blog.content,
-                // userId:1
-            })
+            this.$axios.post('https://my-vuedemo.firebaseio.com/posts.json',this.blog
+            // {
+            //     title:this.blog.title,
+            //     content:this.blog.content,
+            //     // userId:1
+            // }
+            )
             .then(function(data){
                 that.submmited = true
             })
